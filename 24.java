421fd8078ba8b2738093b07195fa60d7c6dc5250
MD Shoaib Ali <shoaib.ali@example.com>
2025-01-26 00:06:20 +0530

    Initial commit

---
+++ 24.java
@@ -0,0 +1,31 @@
+// User function template for JAVA
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+class Solution {
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        Node slow=head; // slow pointer
+        Node fast=head;  // fast pointer
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+            
+            
+        }
+        return false;
+    }
+}
---
+++ 25.java
@@ -0,0 +1,34 @@
+/* class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+        Node slow=head; // slow pointer
+        Node fast=head;  // fast pointer
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+            
+            
+        }
+        return null;
+    }
+}
